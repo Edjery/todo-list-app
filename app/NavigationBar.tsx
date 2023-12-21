3b876363de811ebab114829f32bc9f47bf6837a5
Edjery Gumbao <edisgumbao@gmail.com>
2023-12-21 17:23:19 +0800

    added sidebar

--- app/NavigationBar.tsx
+++ app/NavigationBar.tsx
@@ -1,5 +1,7 @@
+"use client";
 import React from "react";
 import SideMenu from "./components/SideMenu";
+import { Typography } from "@material-ui/core";
 
 const NavigationBar = () => {
   return (

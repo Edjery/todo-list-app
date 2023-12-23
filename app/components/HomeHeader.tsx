503ed88f73947478f15b629b8f214f92fe9b0b47
Edjery Gumbao <edisgumbao@gmail.com>
2023-12-23 12:27:05 +0800

    improved HomeHeader

--- app/components/HomeHeader.tsx
+++ app/components/HomeHeader.tsx
@@ -3,34 +3,41 @@
 import AddCircleIcon from "@material-ui/icons/AddCircle";
 import SearchIcon from "@material-ui/icons/Search";
 import SortIcon from "@material-ui/icons/Sort";
-import { Box, Button, Container, IconButton, Typography } from "../lib/MUI-v4";
+import {
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "../lib/MUI-v4";
 
 const HomeHeader = () => {
   return (
-    <Container className="text-center mt-10">
-      <Box className="mr-40 inline-block">
+    <Container maxWidth="md">
+      <Toolbar className="flex justify-center mt-10 shadow-md rounded-full">
         <IconButton>
           <SearchIcon />
         </IconButton>
-      </Box>
 
-      <Box
-        className="mr-40 px-5 bg-[#998767] inline-block rounded-full"
-        boxShadow={5}
-      >
-        <Button>
-          <AddCircleIcon className="mr-5 text-white" />
-          <Typography variant="h5" className="text-white normal-case">
-            Add Task
-          </Typography>
-        </Button>
-      </Box>
+        <Box className="mx-32">
+          <IconButton color="primary" size="small">
+            <Box className="flex px-5 bg-[#998767] rounded-full shadow-md py-1">
+              <AddCircleIcon className="mr-3 text-white self-center" />
+              <Typography
+                variant="h6"
+                className="text-white self-center inline"
+              >
+                Add Task
+              </Typography>
+            </Box>
+          </IconButton>
+        </Box>
 
-      <Box className="inline-block">
         <IconButton>
           <SortIcon />
         </IconButton>
-      </Box>
+      </Toolbar>
     </Container>
   );
 };

781467f6999bb6ef25360fcfc8bcd0ecd6479ab0
Edjery Gumbao <edisgumbao@gmail.com>
2024-01-19 13:39:51 +0800

    Refactor PopupAlert

--- app/components/Task/common/PopupAlert.tsx
+++ app/components/Task/common/PopupAlert.tsx
@@ -14,20 +14,16 @@ interface Props {
 
 const PopupAlert = ({ message, open, onClose }: Props) => {
   return (
-    <Box>
-      <Dialog open={open} onClose={onClose} maxWidth="lg">
-        <DialogContent>
-          <Typography>{message}</Typography>
-        </DialogContent>
-        <Box>
-          <Box component="div" className="flex justify-end mx-5 mb-3">
-            <Button variant="contained" color="primary" onClick={onClose}>
-              Confirm
-            </Button>
-          </Box>
-        </Box>
-      </Dialog>
-    </Box>
+    <Dialog open={open} onClose={onClose} maxWidth="lg">
+      <DialogContent>
+        <Typography>{message}</Typography>
+      </DialogContent>
+      <Box component="div" className="flex justify-end mx-5 mb-3">
+        <Button variant="contained" color="primary" onClick={onClose}>
+          Confirm
+        </Button>
+      </Box>
+    </Dialog>
   );
 };
 
